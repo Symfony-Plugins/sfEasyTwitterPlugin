f2390152cc411f9efb655d52e82fe96d2e9c4b8f
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-05 10:30:29 -0500

    [sfEasyTwitterPlugin] imported base plugin structure
    git-svn-id: http://svn.symfony-project.com/plugins/sfEasyTwitterPlugin/trunk@18969 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfEasyTwitterPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfEasyTwitterPlugin configuration.
+ * 
+ * @package     sfEasyTwitterPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id$
+ */
+class sfEasyTwitterPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
